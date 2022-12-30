7aefd25a9a43b853529d472eb6bbab8d474197cd
Mehdi Sellami <sellamimehdi87@gmail.com>
2022-12-30 08:43:32 -0800

    Adding Two class TextBox && FirstProjet

---
+++ src/FirstProject.java
@@ -0,0 +1,14 @@
+public class FirstProject {
+    public static void main(String[] args){
+
+        var textBox1 = new TextBox();
+        textBox1.setText("Box1");
+        System.out.println(textBox1.text);
+
+        var textBox2 = textBox1;
+        textBox2.setText("Hello Hello");
+        System.out.println(textBox2.text);
+
+
+    }
+}
---
+++ src/TextBox.java
@@ -0,0 +1,15 @@
+public class TextBox {
+
+    public String text; // Field
+
+    public void setText(String text){
+
+       this.text = text;
+
+    }
+
+    public void clear(){
+
+        text = "";
+    }
+}
