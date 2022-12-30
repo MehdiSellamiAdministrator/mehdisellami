dbf723454c9f3cd66c142fabfac34593c9dbed48
Mehdi Sellami <sellamimehdi87@gmail.com>
2022-12-30 10:00:43 -0800

    Modifier WorkBook 1.1 && Adding YourIntials Class, also print your initials

---
+++ src/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args){
+        System.out.println("M   M  EEEEE");
+        System.out.println("MM MM  E    ");
+        System.out.println("M M M  E    ");
+        System.out.println("M   M  EEE  ");
+        System.out.println("M   M  E    ");
+        System.out.println("M   M  E    ");
+        System.out.println("M   M  EEEEE");
+    }
+}
