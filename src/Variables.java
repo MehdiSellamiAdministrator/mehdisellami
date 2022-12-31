cd3f1dff57ed217e34a44cfce99e47fa85ecf85a
Mehdi Sellami <sellamimehdi87@gmail.com>
2022-12-30 17:59:52 -0800

    Adding && Printing Variables

---
+++ src/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+    public static void main(String[] args){
+
+        // Intro to Variables
+        byte peopleOnBus = 120;
+        short indexNumber = 30405;
+        int people = 100_000;
+        long viewsCount = 100_000_000_000_000L;
+        char firstName = 'M';
+        float price = 10.99F;
+        double timeToFirstLoad = 1.34769898088766;
+
+        // Printing Values
+        System.out.println(peopleOnBus);
+        System.out.println(indexNumber);
+        System.out.println(people);
+        System.out.println(viewsCount);
+        System.out.println(firstName);
+        System.out.println(price);
+        System.out.println(timeToFirstLoad);
+    }
+}
