f40bccc94d1a7516c8fac9f29fcab4db66f93ec8
Mehdi Sellami <sellamimehdi87@gmail.com>
2022-12-31 19:52:15 -0800

    Update Math Operations

--- src/MathOperations.java
+++ src/MathOperations.java
@@ -23,5 +23,18 @@ public class MathOperations {
         System.out.println("Originally, Fred had " + (bagOfSweets2 / bagOfSweets1) + " times more sweets than George.");
         System.out.println("Andromeda has " + (starsInAndromeda / starsInMilkyWay) + " times more stars.");
         System.out.println("Snaps caught the error. Furious, he divided Ron's score to: " + (testScore / bonusMarks) + ".");
+
+        System.out.println("10 is an even number, since dividing 10 by 2 has a remainder of: " + (10 % 2));
+        System.out.println("5 is an odd number, since dividing 5 by 2 has a remainder of: " + (5 % 2));
+
+        int counter = 0;
+        counter++;
+        counter++;
+        counter++;
+        System.out.println("I count: " + counter);
+        counter--;
+        counter--;
+        counter--;
+        System.out.println("I count: " + counter);
     }
 }
